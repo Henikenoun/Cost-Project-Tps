9a530af5eafe163593589fbe1204e24bf6c7f6b6
HeniKenoun <kenounheni4@gmail.com>
2024-07-18 10:16:14 +0100

    version complete login

---
+++ src/main/java/com/example/demos/user/UserService.java
@@ -0,0 +1,68 @@
+package com.example.demos.user;
+
+import com.example.demos.role.Role;
+import com.example.demos.role.RoleNotFoundException;
+import com.example.demos.role.RoleRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+
+
+
+    public User getUserById(Integer id) {
+        return userRepository.findById(id).orElseThrow(() -> new UserNotFoundException(id));
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User updateUser(Integer id, User updatedUser) {
+        User user = userRepository.findById(id).orElseThrow(() -> new UserNotFoundException(id));
+
+        if (updatedUser.getFirstname() != null) {
+            user.setFirstname(updatedUser.getFirstname());
+        }
+        if (updatedUser.getLasttname() != null) {
+            user.setLasttname(updatedUser.getLasttname());
+        }
+        if (updatedUser.getDateOfBirth() != null) {
+            user.setDateOfBirth(updatedUser.getDateOfBirth());
+        }
+        if (updatedUser.getPhone() != null) {
+            user.setPhone(updatedUser.getPhone());
+        }
+
+        return userRepository.save(user);
+    }
+    @Transactional
+    public User changeUserRole(Integer userId, Integer roleId) {
+        Optional<User> userOptional = userRepository.findById(userId);
+        Optional<Role> roleOptional = roleRepository.findById(roleId);
+
+        if (userOptional.isPresent() && roleOptional.isPresent()) {
+            User user = userOptional.get();
+            Role role = roleOptional.get();
+            user.getRoles().clear(); // Remove all existing roles
+            user.getRoles().add(role); // Add the new role
+            return userRepository.save(user);
+        }
+
+        throw new IllegalArgumentException("User or Role not found");
+    }
+    public void deleteUser(Integer id) {
+        userRepository.deleteById(id);
+    }
+
+
+}
